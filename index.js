de54a5f9202e8c9953f3a0b62056f9179026b1a2
babanat <babanat3@gmail.com>
2024-08-21 11:34:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 class BankAccount {
+  #balance;
   constructor(balance) {
-    this.balance = balance;
+    this.#balance = balance;
   }
 
   getBalance() {
-    return this.balance;
+    return this.#balance;
   }
 
   depositCash(money) {
-    this.balance += money;
+    this.#balance += money;
     return this.getBalance();
   }
   withdraw(money) {
-    if (this.balance < money) {
+    if (this.#balance < money) {
       console.log("There is not enough money in your account");
       return this.getBalance();
     }
-    this.balance -= money;
+    this.#balance -= money;
     return this.getBalance();
   }
 }
 const account1 = new BankAccount(1000);
-
 console.log(account1.getBalance()); // 1000
 account1.depositCash(500);
 console.log(account1.getBalance()); // 1500
+
 account1.withdraw(200);
 console.log(account1.getBalance()); // 1300
